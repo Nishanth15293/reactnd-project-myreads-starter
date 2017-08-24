0753b8085f88a04da6b2813c1b5ad6472d29eea6
Nishanth15293 <777.nishanth@gmail.com>
2017-08-23 21:48:56 -0400

    feat: fetching from API

---
+++ src/Components/Book.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import * as BooksAPI from '../BooksAPI'
+
+class Book extends Component {
+    constructor(){
+        super();
+    }
+
+    state = {
+        shelf : 'none'
+    }
+
+    handleChange = (e) => {
+        BooksAPI.update(this.props.Book, e.target.value).then((book) => {
+            console.log(book.shelf);
+            this.setState({shelf: book.shelf});
+        })
+    }
+
+    render(){
+        const {book} = this.props
+        return(
+            <li key={book.id}>
+                <div className="book">
+                <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks && book.imageLinks.thumbnail})` }}></div>
+                    <div className="book-shelf-changer">
+                    <select value={this.state.shelf} onChange={this.handleChange}>
+                        <option value="none" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                    </select>
+                    </div>
+                </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{
+                    (book.authors) && book.authors.map((author) => (
+                    <p key={author}>
+                        {author}
+                    </p>
+                ))}</div>
+                </div>
+            </li>
+        )
+    }
+}
+
+export default Book
---
+++ src/Components/BookShelf.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Book from './Book'
+class BookShelf extends Component {
+
+    state = {
+        books : []
+    }
+
+    render(){
+        const {books, title} = this.props
+        const filteredBooks = books.filter((book)=>{
+            return book.shelf == this.props.shelf
+        })
+        return(
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">{title}</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {filteredBooks.map((book)=>(
+                            <div key={book.id}>
+                                <Book book={book}/>
+                            </div> 
+                        ))}
+                    </ol>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default BookShelf
